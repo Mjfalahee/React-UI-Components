ff77a4860c8065299ade46c52fe98f24f0622957
mjfalahee <mjfalahee@gmail.com>
2019-04-23 14:56:39 -0700

    Working on the skeleton of my components

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,14 @@
 import React from 'react';
-import './Button.css';
+import './Button.scss';
+
+
+
+const NumberButton = props => {
+    return (
+        <div>
+        <button className="numberbutton"> {props.number} </button>
+        </div>
+    );
+}
+
+export default NumberButton;
---
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/Output.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Display.scss';
+
+
+function Output() {
+    return (
+        <div className="output">
+            0
+        </div>
+    );
+}
+
+export default Output;
